441b0d22e0a652b13d621d2882536cf152e36097
denispyr <pyr.school@gmail.com>
2021-01-14 20:07:58 +0200

    Saving for attendees

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { Client } = require('pg')
 const express = require("express")
 
 app = express();
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
 
 const client = new Client({
     user: 'postgres',
@@ -12,6 +14,37 @@ const client = new Client({
 })
 
 client.connect()
+
+
+app.post("/attendees", (req, resp) => {
+    console.log("In /attendeess POST");
+
+    const myQuery = {
+        text: "INSERT INTO attendees (fullname, company, experience, email) VALUES ($1, $2, $3, $4)",
+        values: [req.body.fullname, req.body.company, req.body.experience, req.body.email]
+    }
+
+    client
+        .query(myQuery)
+        .then((resuits) => {
+            console.log("Success!");
+            console.log(resuits.rowCount);
+            resp.writeHead(200, {
+                "Content-Type": "text/json"
+            });
+            resp.write(JSON.stringify("ok"));
+            resp.end();
+        })
+        .catch((error) => {
+            console.log("Ooops!");
+            console.log(error);
+            resp.writeHead(200, {
+                "Content-Type": "text/json"
+            })
+            resp.write(JSON.stringify("Failed"));
+            resp.end();
+        });
+})
 
 app.get("/attendees", (req, resp) => {
     let filterName = req.query.filterName;
