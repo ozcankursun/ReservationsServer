903b1f6cfbc0b42f19394ab90333fe7bbc9480b6
denispyr <pyr.school@gmail.com>
2021-01-15 19:52:05 +0200

    Update attendees using PUT method

--- server.js
+++ server.js
@@ -85,6 +85,40 @@ app.post("/attendees", (req, resp) => {
         });
 })
 
+app.put("/attendees", (req, resp) => {
+    console.log("In /attendeess PUT");
+
+
+    // req.body.id
+    const myQuery = {
+        text: "UPDATE attendees SET fullname = $2, experience = $4, email = $5, company = $3 WHERE id = $1",
+        values: [req.body.id, req.body.fullname, req.body.company, req.body.experience, req.body.email]
+    }
+
+    client
+        .query(myQuery)
+        .then((resuits) => {
+            console.log("Success!");
+            console.log(resuits.rowCount);
+            resp.writeHead(200, {
+                "Content-Type": "text/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            resp.write(JSON.stringify("ok"));
+            resp.end();
+        })
+        .catch((error) => {
+            console.log("Ooops!");
+            console.log(error);
+            resp.writeHead(200, {
+                "Content-Type": "text/json",
+                "Access-Control-Allow-Origin": "*",
+            })
+            resp.write(JSON.stringify("Failed"));
+            resp.end();
+        });
+})
+
 app.get("/attendees", (req, resp) => {
     let filterName = req.query.name ? req.query.name : "";
     console.log(filterName);  
